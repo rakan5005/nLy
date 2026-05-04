cab27371158dc52965821437bcac474e7954acea
nLy Dev <admin@nly.local>
2026-05-05 02:21:22 +0300

    Add setup.bat and nLy.bat for 1-click install and run

---
+++ nLy.bat
@@ -0,0 +1,2 @@
+@echo off
+python -m by_nly
---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ========================================
+echo      nLy - Installing...
+echo ========================================
+echo.
+pip install -e .
+echo.
+echo Done! You can now run nLy.
+echo.
+pause
